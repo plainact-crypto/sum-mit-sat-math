0c5a776b974ee1c12419523d723581288c13774d
plainact-crypto <plainact@gmail.com>
2026-09-13 15:16:06 +0300

    Wire Factoring Trinomials English video

--- video-variables-explainer.js
+++ video-variables-explainer.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+require('./video-factoring-trinomials-explainer.js');
+
 const dist = path.join(__dirname, 'dist');
 const lesson = 'Variables on Both Sides';
 const marker = 'Video Explanation · English';
