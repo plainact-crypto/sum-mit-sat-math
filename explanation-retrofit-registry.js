dc099026f08d14f502ddc870b82242a990fb2816
plainact-crypto <plainact@gmail.com>
2026-09-20 01:58:19 +0300

    Classify Range explanation

--- explanation-retrofit-registry.js
+++ explanation-retrofit-registry.js
@@ -1,2 +1,2 @@
 const registry=require('./explanation-retrofit-registry-before-functions-from-tables');
-module.exports=[...registry,{slug:'functions-from-tables',lesson:'Functions from Tables',classification:'NEITHER',graph:null,desmos:null}];
+module.exports=[...registry,{slug:'functions-from-tables',lesson:'Functions from Tables',classification:'NEITHER',graph:null,desmos:null},{slug:'range',lesson:'Range',classification:'NEITHER',graph:null,desmos:null}];
