89f74a67dea2cc7148c23afb8419336f2464c09c
plainact-crypto <plainact@gmail.com>
2026-09-24 17:46:31 +0300

    Publish Converting Between Representations lesson package

--- content-worker2-next.js
+++ content-worker2-next.js
@@ -39,3 +39,5 @@ require('./axis-of-symmetry-content-build');
 require('./video-axis-of-symmetry-explainer');
 require('./number-of-intersections-content-build');
 require('./video-number-of-intersections-explainer');
+require('./converting-between-representations-content-build');
+require('./video-converting-between-representations-explainer');
