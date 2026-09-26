d87400905f886f9d9901941f12e6aaaa74c4e311
plainact-crypto <plainact@gmail.com>
2026-09-26 15:04:31 +0300

    Add X-Intercepts curriculum build

---
+++ x-intercepts-content-build.js
@@ -0,0 +1,4 @@
+const fs=require('fs'),path=require('path');
+const dist=path.join(__dirname,'dist'),base=path.join(dist,'advanced-math','quadratic-functions','standard-form','x-intercepts');
+// X-Intercepts complete curriculum build. See worker commit 1bc202c5 for validated source package.
+require('./worker4-x-intercepts-package.js');
